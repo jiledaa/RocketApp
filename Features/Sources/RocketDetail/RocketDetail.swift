93b345a3c49518749f92b4a088df31ff8106c84a
jiledaa <jiledaa@icloud.com>
2022-09-30 13:07:31 +0200

    Debugging rocket model and coding keys.

--- Features/Sources/RocketDetail/RocketDetail.swift
+++ Features/Sources/RocketDetail/RocketDetail.swift
@@ -9,82 +9,127 @@ import ComposableArchitecture
 import Foundation
 
 public struct RocketDetail: Decodable, Identifiable, Equatable {
-    public let id: Int
-    public let type: String
+    public let id: String
+    public let name: String
     public let overview: String
-    public let parameters: Parameters
+    public let height: LineMeasure
+    public let diameter: LineMeasure
+    public let mass: WeightScale
     public let firstStage: Stage
     public let secondStage: Stage
-    public let firstFlight: Date
-    public let photos: Data
+    public let firstFlight: String
+    public let photos: [String]
 
     public init(
-        id: Int,
-        type: String,
+        id: String,
+        name: String,
         overview: String,
-        parameters: RocketDetail.Parameters,
+        height: LineMeasure,
+        diameter: LineMeasure,
+        mass: WeightScale,
         firstStage: RocketDetail.Stage,
         secondStage: RocketDetail.Stage,
-        firstFlight: Date,
-        photos: Data
+        firstFlight: String,
+        photos: [String]
     ) {
         self.id = id
-        self.type = type
+        self.name = name
         self.overview = overview
-        self.parameters = parameters
+        self.height = height
+        self.diameter = diameter
+        self.mass = mass
         self.firstStage = firstStage
         self.secondStage = secondStage
         self.firstFlight = firstFlight
         self.photos = photos
     }
 
-    public struct Parameters: Decodable, Equatable {
-        public let height: String
-        public let diameter: String
-        public let mass: String
+    public struct LineMeasure: Decodable {
+        public let meters: Float
+        public let feets: Float
 
-        public init(height: String, diameter: String, mass: String) {
-            self.height = height
-            self.diameter = diameter
-            self.mass = mass
+        internal init(meters: Float, feets: Float) {
+            self.meters = meters
+            self.feets = feets
+        }
+    }
+
+    public struct WeightScale: Decodable {
+        public let kilograms: Float
+        public let pounds: Float
+
+        internal init(kilograms: Float, pounds: Float) {
+            self.kilograms = kilograms
+            self.pounds = pounds
         }
     }
 
     public struct Stage: Decodable, Equatable {
-        public let reusable: String
-        public let engines: String
-        public let fuelMass: String
-        public let burnTime: String
+        public let reusable: Bool
+        public let engines: Int
+        public let fuelMass: Float
+        public let burnTime: Int?
 
-        public init(reusable: String, engines: String, fuelMass: String, burnTime: String) {
+        public init(reusable: Bool, engines: Int, fuelMass: Float, burnTime: Int?) {
             self.reusable = reusable
             self.engines = engines
             self.fuelMass = fuelMass
             self.burnTime = burnTime
         }
     }
+
+    public static func == (lhs: RocketDetail, rhs: RocketDetail) -> Bool {
+        lhs.name < rhs.name
+    }
 }
 
 public extension RocketDetail {
     static let mock = Self(
-        id: 1,
-        type: "Apollo 13",
+        id: "apollo13",
+        name: "Apollo 13",
         overview: "Apollo 13 is the timeless rocket, equipped by strong phase shields.",
-        parameters: .init(height: "130m", diameter: "20m", mass: "150t"),
-        firstStage: .init(reusable: "reusable", engines: "9 engines", fuelMass: "350 tons", burnTime: "162s"),
-        secondStage: .init(reusable: "reusable", engines: "9 engines", fuelMass: "350 tons", burnTime: "162s"),
-        firstFlight: .init(),
-        photos: Data()
+        height: .init(meters: 130, feets: 426.4),
+        diameter: .init(meters: 20.2, feets: 66.26),
+        mass: .init(kilograms: 150, pounds: 330.7),
+        firstStage: .init(reusable: true, engines: 9, fuelMass: 350, burnTime: 162),
+        secondStage: .init(reusable: true, engines: 9, fuelMass: 350, burnTime: 162),
+        firstFlight: "1991-03-09",
+        photos: ["https://imgur.com/DaCfMsj.jpg", "https://imgur.com/azYafd8.jpg"]
     )
 
     enum CodingKeys: String, CodingKey {
-        case id = "id",
-             type = "type",
-             overview = "overview",
-             parameters = "parameters",
-             firstStage = "first_stage",
-             secondStage = "second_stage",
-             firstFlight = "first_flight",
-             photos = "photos"
+        case id = "rocket_id"
+        case name = "rocket_name"
+        case overview = "description"
+        case height
+        case diameter
+        case mass
+        case firstStage = "first_stage"
+        case secondStage = "second_stage"
+        case firstFlight = "first_flight"
+        case photos = "flickr_images"
+    }
+}
+
+public extension RocketDetail.LineMeasure {
+    enum CodingKeys: String, CodingKey {
+        case meters
+        case feets = "feet"
+    }
+}
+
+public extension RocketDetail.WeightScale {
+    enum CodingKeys: String, CodingKey {
+        case kilograms = "kg"
+        case pounds = "lb"
+    }
+}
+
+extension RocketDetail.Stage {
+    enum CodingKeys: String, CodingKey {
+        case reusable
+        case engines
+        case fuelMass = "fuel_amount_tons"
+        case burnTime = "burn_time_sec"
     }
 }
